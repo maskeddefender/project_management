e1d0861877622760bedd72c72f6c7d1baadd764c
YashviiSharma <yashvi@korecent.com>
2025-03-10 15:34:09 +0530

    add client feedback

---
+++ project_management/project_management/doctype/client_feedback/client_feedback.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aman and Yashvi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ClientFeedback(Document):
+	pass
