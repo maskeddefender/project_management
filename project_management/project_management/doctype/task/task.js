4fa707169b880db5a19d0e39195239d9c55cce80
maskeddefender <amanupadhayay2906@gmail.com>
2025-04-19 01:04:26 +0530

    Added timer and progress bar in the task

--- project_management/project_management/doctype/task/task.js
+++ project_management/project_management/doctype/task/task.js
@@ -1,8 +1,200 @@
 // Copyright (c) 2025, Aman and Yashvi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Task", {
-// 	refresh(frm) {
+frappe.ui.form.on("Task", {
+    refresh: function(frm) {
+        if(!frm.is_new()) {
+            frm.trigger("make_dashboard");
+            
+            if (!frm.progress_bar_initialized) {
+                frm.trigger("show_progress_bar");
+                frm.progress_bar_initialized = true;
+            } else {
+                frm.trigger("update_progress_bar");
+            }
+        }
 
-// 	},
-// });
+        // Disable start button if task is 100% complete
+        if(frm.doc.progress_ === 100) {
+            frm.page.set_indicator(__("Completed"), "green");
+            frm.disable_save();
+        }
+    },
+
+    show_progress_bar: function(frm) {
+        // Create progress bar container only once
+        let progress_html = `
+            <div class="custom-progress-container" style="margin-bottom: 15px;">
+                <div class="progress" style="height: 20px;">
+                    <div class="progress-bar custom-task-progress" role="progressbar" 
+                        aria-valuemin="0" 
+                        aria-valuemax="100">
+                    </div>
+                </div>
+            </div>
+        `;
+        
+        let section_break = $(frm.fields_dict.section_break_ard0.wrapper);
+        if(section_break.length && !section_break.prev().hasClass('custom-progress-container')) {
+            section_break.before(progress_html);
+        }
+        
+        frm.trigger("update_progress_bar");
+    },
+
+    update_progress_bar: function(frm) {
+        let progress = frm.doc.progress_ || 0;
+        
+        // Determine color based on progress
+        let progressColor;
+        if (progress < 10) {
+            progressColor = '#dc3545'; // red
+        } else if (progress >= 10 && progress < 20) {
+            progressColor = '#d63384'; // pink
+        } else if (progress >= 20 && progress < 50) {
+            progressColor = '#fd7e14'; // orange
+        } else if (progress >= 50 && progress < 80) {
+            progressColor = '#ffc107'; // yellow-orange
+        } else if (progress >= 80 && progress < 100) {
+            progressColor = '#20c997'; // light green
+        } else if (progress >= 100) {
+            progressColor = '#198754'; // dark green
+        }
+
+        let progress_bar = $(frm.wrapper).find('.custom-task-progress');
+        if (progress_bar.length) {
+            progress_bar.css({
+                'width': progress + '%',
+                'background-color': progressColor
+            }).text(progress + '%');
+            
+            progress_bar.attr('aria-valuenow', progress);
+        }
+    },
+
+    make_dashboard: function(frm) {
+        if(frm.doc.__islocal) return;
+
+        let currentIncrement = 0;
+        let interval;
+
+        function updateStopwatch(increment) {
+            const hours = Math.floor(increment / 3600);
+            const minutes = Math.floor((increment % 3600) / 60);
+            const seconds = Math.floor(increment % 60);
+            
+            $(frm.wrapper).find(".stopwatch .hours").text(hours.toString().padStart(2, '0'));
+            $(frm.wrapper).find(".stopwatch .minutes").text(minutes.toString().padStart(2, '0'));
+            $(frm.wrapper).find(".stopwatch .seconds").text(seconds.toString().padStart(2, '0'));
+        }
+
+        // Only add timer if it doesn't exist
+        if (!$(frm.wrapper).find('.stopwatch').length) {
+            const timer = `
+                <div class="stopwatch" style="font-weight:bold;margin:0px 13px 0px 2px;
+                    color:#545454;font-size:18px;display:inline-block;vertical-align:text-bottom;">
+                    <span class="hours">00</span>
+                    <span class="colon">:</span>
+                    <span class="minutes">00</span>
+                    <span class="colon">:</span>
+                    <span class="seconds">00</span>
+                </div>`;
+            
+            frm.toolbar.page.add_inner_message(timer);
+        }
+
+        currentIncrement = frm.events.get_current_time(frm);
+        updateStopwatch(currentIncrement);
+
+        frm.page.clear_actions_menu();
+
+        if(!frm.doc.actual_start_date && frm.doc.progress_ < 100) {
+            frm.add_custom_button(__("Start Task"), () => frm.events.start_timer(frm));
+        } else if(frm.doc.status === "In Progress") {
+            frm.add_custom_button(__("Pause"), () => frm.events.pause_timer(frm));
+            frm.add_custom_button(__("Complete"), () => frm.events.complete_task(frm));
+        } else if(frm.doc.status === "Paused") {
+            frm.add_custom_button(__("Resume"), () => frm.events.resume_timer(frm));
+        }
+
+        // Update timer if task is running
+        if(frm.doc.status === "In Progress") {
+            currentIncrement = frm.events.get_current_time(frm);
+            interval = setInterval(() => {
+                currentIncrement++;
+                updateStopwatch(currentIncrement);
+            }, 1000);
+        }
+
+        $(document).on("form-refresh", () => clearInterval(interval));
+    },
+
+    start_timer: function(frm) {
+        frm.call('start_timer')
+            .then(() => frm.reload());
+    },
+
+    pause_timer: function(frm) {
+        let current_progress = frm.doc.progress_ || 0;
+        
+        frappe.prompt({
+            fieldtype: "Float",
+            label: __("Completed Percentage in this Session"),
+            fieldname: "completed_percent",
+            reqd: 1,
+            default: current_progress,
+            description: __("Current total completion: {0}%", [current_progress])
+        }, (data) => {
+            if (data.completed_percent < 0) {
+                frappe.throw(__("Percentage cannot be negative"));
+            }
+            
+            frm.dashboard.set_headline_alert("Updating task progress...");
+            
+            frm.call('pause_timer', data)
+                .then(() => {
+                    frm.reload();
+                    frappe.show_alert(__("Task paused"), 3);
+                })
+                .catch(() => {
+                    frm.dashboard.clear_headline();
+                });
+        }, __("Update Progress"), __("Pause"));
+    },
+
+    resume_timer: function(frm) {
+        frm.call('resume_timer')
+            .then(() => frm.reload());
+    },
+
+    complete_task: function(frm) {
+        frappe.confirm(
+            __("Are you sure you want to mark this task as complete?"),
+            function() {
+                frm.dashboard.set_headline_alert("Completing task...");
+                
+                frm.call('complete_task')
+                    .then(() => {
+                        frm.reload();
+                        frappe.show_alert(__("Task marked as complete"), 3);
+                    })
+                    .catch(() => {
+                        frm.dashboard.clear_headline();
+                    });
+            },
+            __("Confirm Completion")
+        );
+    },
+
+    get_current_time: function(frm) {
+        let total_seconds = 0;
+        (frm.doc.time_logs || []).forEach(log => {
+            if(log.from_time && log.to_time) {
+                total_seconds += moment(log.to_time).diff(moment(log.from_time), 'seconds');
+            } else if(log.from_time) {
+                total_seconds += moment().diff(moment(log.from_time), 'seconds');
+            }
+        });
+        return total_seconds;
+    }
+});
--- project_management/project_management/doctype/task/task.py
+++ project_management/project_management/doctype/task/task.py
@@ -1,9 +1,126 @@
 # Copyright (c) 2025, Aman and Yashvi and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.utils.nestedset import NestedSet
+import frappe
+from frappe.utils import now, get_datetime, time_diff_in_seconds, flt
+from frappe.model.document import Document
 
+class Task(Document):
+    def validate(self):
+        self.update_status()
+        self.calculate_totals()
+        
+    def update_status(self):
+        if self.progress_ >= 100:
+            self.status = "Completed"
+        elif self.has_valid_operation():
+            self.status = "In Progress"
+        elif self.actual_start_date:
+            self.status = "Paused"
+        else:
+            self.status = "Planned"
 
-class Task(NestedSet):
-	pass
+    def has_valid_operation(self):
+        return any(log for log in (self.time_logs or []) if not log.to_time)
+
+    def calculate_totals(self):
+        total_mins = 0
+        total_completion = 0
+        
+        for log in (self.time_logs or []):
+            if log.from_time and log.to_time:
+                duration = time_diff_in_seconds(log.to_time, log.from_time)
+                log.time_in_mins = flt(duration / 60, 2)
+                total_mins += log.time_in_mins
+                
+            if hasattr(log, 'completed__of_task') and log.completed__of_task:
+                total_completion += log.completed__of_task
+                
+        self.time_in_minutes = total_mins
+        self.progress_ = min(total_completion, 100)
+
+    @frappe.whitelist()
+    def start_timer(self):
+        if not self.assigned_to:
+            frappe.throw("This task is not assigned to any member. Please assign a member first")
+            
+        if not self.actual_start_date:
+            self.actual_start_date = now()
+
+        self.append("time_logs", {
+            "member": self.assigned_to,
+            "from_time": now(),
+            "completed__of_task": 0
+        })
+        self.status = "In Progress"
+        self.save()
+        return self
+
+    @frappe.whitelist()
+    def pause_timer(self, completed_percent):
+        completed_percent = flt(completed_percent)
+        
+        if not self.time_logs:
+            frappe.throw("No time logs found to pause")
+        
+        previous_completion = sum(
+            log.completed__of_task for log in self.time_logs[:-1] 
+            if hasattr(log, 'completed__of_task') and log.completed__of_task
+        )
+        
+        # Validate that new completion won't exceed 100%
+        if previous_completion + completed_percent > 100:
+            frappe.throw(
+                f"Total completion will exceed 100%. Current completion: {previous_completion}%, " +
+                f"you can add maximum {100 - previous_completion}%"
+            )
+        
+        last_log = self.time_logs[-1]
+        last_log.to_time = now()
+        last_log.completed__of_task = completed_percent
+        
+        if last_log.from_time:
+            duration = time_diff_in_seconds(last_log.to_time, last_log.from_time)
+            last_log.time_in_mins = flt(duration / 60, 2)
+        
+        self.status = "Paused"
+        self.calculate_totals()
+        self.save()
+        return self
+
+    @frappe.whitelist()
+    def resume_timer(self):
+        self.append("time_logs", {
+            "member": self.assigned_to,
+            "from_time": now(),
+        })
+        self.status = "In Progress"
+        self.save()
+        return self
+
+    @frappe.whitelist()
+    def complete_task(self):
+        if not self.time_logs:
+            frappe.throw("No time logs found to complete")
+        
+        # Calculate remaining percentage to reach 100%
+        previous_completion = sum(
+            log.completed__of_task for log in self.time_logs[:-1] 
+            if hasattr(log, 'completed__of_task') and log.completed__of_task
+        )
+        remaining_percentage = 100 - previous_completion
+        
+        last_log = self.time_logs[-1]
+        last_log.to_time = now()
+        last_log.completed__of_task = remaining_percentage
+        
+        if last_log.from_time:
+            duration = time_diff_in_seconds(last_log.to_time, last_log.from_time)
+            last_log.time_in_mins = flt(duration / 60, 2)
+        
+        self.actual_end_date = now()
+        self.status = "Completed"
+        self.progress_ = 100
+        self.calculate_totals()
+        self.save()
+        return self
